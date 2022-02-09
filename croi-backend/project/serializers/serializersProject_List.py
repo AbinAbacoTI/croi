19b5cacd0a9689a2b7aa0db231106e557c9d19a5
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2022-02-09 10:49:34 -0500

    Filtrado-Detallado

---
+++ croi-backend/project/serializers/serializersProject_List.py
@@ -0,0 +1,31 @@
+from  rest_framework import serializers
+from project.models.project import *
+from project.serializers.serializersCategory import *
+from project.serializers.serializersRequestForm import *
+
+class ProjectSerializer(serializers.ModelSerializer):
+    #request_integer =RequestFormSerializer()
+    #category = CategorySerializer()
+
+    request_integer = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
+    #image_url = serializers.CharField(source='get_absolute_image_url')
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'category': instance.category.name_category,
+            'name_project': instance.request_integer.name_project,
+            'description': instance.request_integer.description,
+            'address': instance.request_integer.address,
+            'image': "http://127.0.0.1:8000/media/"+str(instance.request_integer.image),
+            'name_biznes': instance.request_integer.name_biznes,
+            'date': instance.request_integer.date,
+            'state': instance.state,
+            'type_financing': instance.type_financing
+
+        }
+
--- croi-backend/project/views/viewsProject.py
+++ croi-backend/project/views/viewsProject.py
@@ -1,7 +1,3 @@
-from re import search
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, filters
 from project.serializers.serializersProject import ProjectSerializer
 from rest_framework import viewsets
 from project.models import *
@@ -9,5 +5,3 @@ from project.models import *
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
-    filters_backends = (filters.SearchFilter,)
-    search_fields = ('name', 'state',)
